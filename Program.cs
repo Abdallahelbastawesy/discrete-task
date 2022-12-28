fdcb38c0a1d829ba673407af66d0620ece8b73f3
Abdallahelbastawesy <121408660+Abdallahelbastawesy@users.noreply.github.com>
2022-12-28 23:54:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int sum = 0;
+
+Console.WriteLine("please enter the start num");
+
+int start = int.Parse(Console.ReadLine());
+
+Console.WriteLine("please enter the end num");
+
+int end = int.Parse(Console.ReadLine());
+
+for (int i = start; i < end; i++)
+{
+    for (int j = 1; i > j; j++)
+    {
+        if (i % j == 0)
+        {
+            sum = sum + j;
+        }
+    }
+    if (sum == i)
+    {
+        Console.WriteLine(i);
+    }
+    sum = 0;
+
+}
+
+
+
+
+
+
